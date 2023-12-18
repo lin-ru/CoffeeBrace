82d58d28365972218bfe71c3dbd8d534d6769b97
Lina Rusakova <145145991+lin-ru@users.noreply.github.com>
2023-12-18 03:07:07 +0300

    Initial commit

---
+++ src/main/java/com/github/linru/CoffeeBrace/CoffeeBraceApplication.java
@@ -0,0 +1,13 @@
+package com.github.linru.CoffeeBrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeBraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/linru/CoffeeBrace/CoffeeBraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.linru.CoffeeBrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
