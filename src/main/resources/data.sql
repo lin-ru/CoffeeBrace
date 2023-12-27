c5dae9d439852320f0627b72ff64617ef9b584bd
Lina Rusakova <145145991+lin-ru@users.noreply.github.com>
2023-12-28 02:24:08 +0300

    initial commit for dev branch. changed application.properties to  application.yaml application.yaml file filled fields about: server port, context-pathh api-001, datasource for users, h2 properties added 2 source files: data.sql and schema.sql (empty)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users
+(
+    id          BIGSERIAL PRIMARY KEY,
+    status      VARCHAR(10),
+    username    VARCHAR(255) UNIQUE,
+    passowrd    VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS roles CASCADE;
+CREATE TABLE roles
+(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+DROP TABLE IF EXISTS user_roles CASCADE;
+CREATE TABLE user_roles
+(
+    user_id BIGINT NOT NULL REFERENCES users,
+    role_id BIGINT NOT NULL REFERENCES roles,
+    unique (user_id, role_id)
+);
