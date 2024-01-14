c2fb26ea926d9a7cd82aaa98c478d0debc92238e
Lina Rusakova <145145991+lin-ru@users.noreply.github.com>
2024-01-14 18:22:02 +0300

    next changes: added db:  for users and roles , added liquibase migration

--- src/main/java/com/github/linru/CoffeeBrace/config/security/jwt/JwtUser.java
+++ src/main/java/com/github/linru/CoffeeBrace/config/security/jwt/JwtUser.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @Getter
-@Setter()
+@Setter
 public class JwtUser implements UserDetails {
 
     private final Long id;
--- src/main/java/com/github/linru/CoffeeBrace/entities/User.java
+++ src/main/java/com/github/linru/CoffeeBrace/entities/User.java
@@ -1,19 +1,10 @@
 package com.github.linru.CoffeeBrace.entities;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -22,8 +13,6 @@ import java.util.List;
 @NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
 @RequiredArgsConstructor
 public class User extends BaseEntity {
-
-    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,12 +30,7 @@ public class User extends BaseEntity {
     private String firstname;
     @Column(name = "last_name")
     private String lastname;
-    private String city;
-    private String state;
-    private String postalcode;
-    private String phoneNumber;
 
-    private String status;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
